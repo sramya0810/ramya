0ce56bdaf00eb531d0b0f8a4115ae153aa59d695
sramya0810 <sramya0810@gmail.com>
2017-06-20 15:42:20 +0530

    Create positivenegavtive.c

---
+++ positivenegavtive.c
@@ -0,0 +1,17 @@
+#include<studio.h>
+void main()
+{
+int n;
+if(n!=0)
+{
+if(n>0)
+{
+printf("number is positive");
+else
+printf("number is negative");
+}
+else
+printf("number is zero");
+}
+getch();
+}
