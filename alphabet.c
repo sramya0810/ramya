fead7aca9eb880e54d115ea530b1e50902454c7b
sramya0810 <sramya0810@gmail.com>
2017-06-28 15:57:43 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -4,11 +4,11 @@
 int main() {  
     char cha;
     
-    printf("Enter a Character\n");  
+    printf("Enter alphabet\n");  
     scanf("%c", &cha);  
        
     if(isalpha(cha)) {  
-        printf("%c is an Alphabet\n", cha);  
+        printf("%c is an alphabet\n", cha);  
     else 
         printf("%c is not an alphabet\n", cha);  
     }
