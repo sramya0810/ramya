be6948f856f81bb3591b672ee3ece6e92e4f0f77
sramya0810 <sramya0810@gmail.com>
2017-06-28 15:40:50 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+ 
+int main()
+{
+    char cha;
+ 
+    printf("Please Enter an alphabet: \n");
+    scanf(" %c", &cha);
+    
+    if(ch == 'a' || cha == 'e' || cha == 'i' || cha == 'o' || cha == 'u' ||
+		ch == 'A' || cha == 'E' || cha == 'I' || cha == 'O' || cha == 'U')  
+		{printf("\n %c is a vowel", cha);
+	
+    else 
+    	printf("\n %c is a consonent", cha);}
+	}
