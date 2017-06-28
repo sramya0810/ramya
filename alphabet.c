08d1bfbeb492c7afe6bd9a86fc37b92d5497ead6
sramya0810 <sramya0810@gmail.com>
2017-06-28 15:58:51 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -1,15 +1,13 @@
 #include <stdio.h> 
-#include <ctype.h> 
-   
-int main() {  
+int main()
+{  
     char cha;
-    
     printf("Enter alphabet\n");  
     scanf("%c", &cha);  
-       
-    if(isalpha(cha)) {  
-        printf("%c is an alphabet\n", cha);  
+    if(isalpha(cha)) 
+    {  
+    printf("%c is an alphabet\n", cha);  
     else 
-        printf("%c is not an alphabet\n", cha);  
+    printf("%c is not an alphabet\n", cha);  
     }
-   }
+}
