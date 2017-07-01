b0350d9f71338497baa785a65e7c227a2e8d5566
sramya0810 <sramya0810@gmail.com>
2017-07-01 16:08:17 +0530

    Create sum of n num.c

---
+++ sum of n num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+ int n, i, sum = 0;
+ printf("Enter a number: ");
+ scanf("%d",&n);
+ for(i=1; i <= n; ++i)
+ {
+ sum = sum+ i;   
+ }
+printf("Sum = %d",sum);
+}
