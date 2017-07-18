17749159215d475c1491b4ae882e909afd499565
sramya0810 <sramya0810@gmail.com>
2017-07-18 18:09:26 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int factorial(int);
+int main()
+{
+    int n,r;
+    printf("Enter a number to find it's Factorial: ");
+    scanf("%d", &n);
+    if (n< 0)
+    {
+        printf("Factorial of negative number not possible\n");
+    }
+    else
+    {
+        r= factorial(n);
+        printf("The Factorial of %d is %d.\n", n, r);
+    }
+    return 0;
+}
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return(n *factorial(n - 1));
+    }
+}
